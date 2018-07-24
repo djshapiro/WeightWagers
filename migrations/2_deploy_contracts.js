ca9b6949070fc6d4934d4e90f013365b5274432d
Daniel Shapiro <dan@Daniels-MacBook-Pro-2.local>
2018-07-24 10:33:50 -0700

    actually migrate WeightWagers contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var WeightWagers = artifacts.require("WeightWagers");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  //deployer.deploy(WeightWagers, { from: accounts[9], gas:6721975, value: 500000000000000000 });
+  deployer.deploy(WeightWagers);
 };
