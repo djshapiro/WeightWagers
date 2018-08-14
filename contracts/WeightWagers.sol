be24c36a3eb8529053d3882931294e16569f17f7
Daniel Shapiro <dan@Daniels-MacBook-Pro-2.local>
2018-08-13 22:06:49 -0400

    In the middle of commenting everything

--- contracts/WeightWagers.sol
+++ contracts/WeightWagers.sol
@@ -3,22 +3,29 @@ pragma solidity ^0.4.24;
 import "installed_contracts/oraclize-api/contracts/usingOraclize.sol";
 import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
 
+/**
+ * @title WeightWagers
+ */
 contract WeightWagers is usingOraclize{
   using SafeMath for uint;
 
-  /**
+  /*
    * Data structures
-   **/
+   */
 
+  // The structure of all wagers
   struct Wager {
     uint expiration; //timestamp after which the contract is deemed "expired"
     uint desiredWeightChange; //amount of weight wagerer wants to lose
-    uint wagerAmount; //amount this person wagered
-    string smartScaleID; //The user's "credentials" for their smart scale
+    uint wagerAmount; //amount of wei this person wagered
+    string smartScaleID; //the user's "credentials" for their smart scale
     address wagerer; //the address of the person making the wager
     uint startWeight; //the starting weight of the wagerer
   }
 
+  /* A structure used temporarily while waiting
+   * for smart scale data to finish verifying
+   */ a wager
   struct VerifyingWager {
     address wagerer; //the address of the person making the wager
     uint wagerIndex; //the index of the wager in the wagers array - we have to
@@ -26,9 +33,9 @@ contract WeightWagers is usingOraclize{
                      //to delete once the wager has been verified
   }
 
-  /**
+  /*
    * Contract variables
-   **/
+   */
 
   //The current reward that users will receive (as a percentage
   //of their wager) after successfully verifying a wager.
@@ -53,46 +60,56 @@ contract WeightWagers is usingOraclize{
   // that we can officially verify the wager 
   mapping(bytes32 => VerifyingWager) private wagersBeingVerified;
 
-  /**
+  /*
    * Modifiers
    */
 
+  // A simple modifier to prevent non-owners from using functions
   modifier isOwner () {
     require (msg.sender == owner);
     _;
   }
 
+  // A simple modifier to prevent functions from running when the contract is stopped.
   modifier notWhenStopped () {
     require (!stopped);
     _;
   }
 
-  /**
+  /*
    * Events
-   **/
+   */
 
   // for when a user attempts to create a wager
   event WagerCreated(uint expiration, uint desiredWeightChange, uint wagerAmount, string smartScaleID);
+
   // for when the oraclized smart scale returns
   // data for a wager that a user is trying to create
   event WagerActivated(address wagerer, uint wagerAmount);
+
   // for when a user attempts to verify a wager
   event WagerBeingVerified(address verifierAddress, uint wagerIndex);
-  // for when a wager is verified
+
+  // for when a wager is verified and the reward is paid out
   event WagerVerified(address wagerer, uint wagerAmount);
-  // for when a wager is expired - called after a user
+
+  // for when a wager is expired - emitted after a user
   // attempts to verify a wager and the contract discovers
   // that the wager has expired
   event WagerExpired(address wagerer, uint wagerAmount);
-  // for when the verify callback recieves a valid
-  // wager but one where the user hasn't actually achieved
-  // their goal weight yet
+
+  // for when the verify callback discovers that the user
+  // hasn't hit their goal weight AND that the wager
+  // hasn't expired
   event WagerUnchanged(bytes32 myid);
 
-  /**
+  /*
    * Functions
-   **/
+   */
 
+  /**
+   * @dev WeightWagers constructor
+   */
   function WeightWagers() payable {
     owner = msg.sender;
     stopped = false;
@@ -100,28 +117,29 @@ contract WeightWagers is usingOraclize{
     OAR = OraclizeAddrResolverI(0x6f485C8BF6fc43eA212E93BBF8ce046C7f1cb475);
   }
 
+  /**
+   * @dev Sets a new reward multiplier for the function. 1031 equals a 3.1% return.
+   * @param newRewardMultpilier Desired new value for the reward multiplier 
+   */
   function setRewardMultiplier(uint newRewardMultiplier) public isOwner {
     rewardMultiplier = newRewardMultiplier;
   }
 
-  /*//DJSFIXME You may not need this because public variables have getters and setters
-  function getRewardMultiplier() public returns (uint rewardMultiplier) {
-    return rewardMultiplier;
-  }*/
-
+  /**
+   * @dev Sets the value of the stopped variable. This decides whether the contract is stopped.
+   * @param newStopped Desired new value for the stopped variable
+   */
   function setStopped(bool newStopped) public isOwner {
     stopped = newStopped;
   }
 
+  /**
+   * @return stopped The stopped variable for the contract
+   * @return rewardMultiplier The current reward multiplier for the contract
+   */
   function getAdminStuff() public view returns (bool, uint) {
     return (stopped, rewardMultiplier);
   }
-
-  /*//DJSFIXME You may not need this because public variables have getters and setters
-  function getStopped() public returns (bool, address) {
-    emit WhoIsOwner(owner, msg.sender);
-    return (stopped, owner);
-  }*/
 
   //The user calls this function when they want to create a wager
   function createWager(uint _expiration, uint _desiredWeightChange, string _smartScaleID) public payable notWhenStopped {
